2667d2ef63b075c46ff017eae01ca5def230557a
Diego Jasso <diego.jasso01@utrgv.edu>
2022-11-21 23:03:01 -0600

    add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (1916851, 0, 61698.4665);
+
+--UPDATE sandbox SET int64_value = 51651 WHERE id = 25;
+
+DELETE FROM sandbox WHERE id = 36;
